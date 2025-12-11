346154771c36257683a8e5f81b29e6975b67d848
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2025-12-11 11:31:47 -0300

    feat: login com adm criado pelo banco funcionando

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # TODO: lógica de autenticação real
-    flash[:notice] = "Login de teste realizado."
-    redirect_to root_path
+    # Aceita email OU matrícula como identificador
+    login_param = params[:login]
+    user = User.find_by("email = ? OR matricula = ?", login_param, login_param)
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Login realizado com sucesso!"
+    else
+      flash.now[:alert] = "Email, matrícula ou senha inválidos."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # TODO: logout real
-    flash[:notice] = "Logout realizado."
-    redirect_to login_path
+    session.delete(:user_id)
+    redirect_to login_path, notice: "Você saiu da sessão."
   end
 end
