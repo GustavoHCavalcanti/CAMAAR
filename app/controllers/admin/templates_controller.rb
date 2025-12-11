526287b6220e6f2def385014bc5dee4f13d2b4e8
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2025-12-11 12:26:03 -0300

    fix: CRUD de turma funcionando

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -1,30 +1,49 @@
 module Admin
   class TemplatesController < ApplicationController
     def index
+      @templates = ::Template.all
     end
 
     def show
+      @template = ::Template.find(params[:id])
     end
 
     def new
+      @template = ::Template.new
     end
 
     def create
-      flash[:notice] = "Template criado (placeholder)"
-      redirect_to admin_templates_path
+      @template = ::Template.new(template_params)
+      if @template.save
+        redirect_to admin_templates_path, notice: "Template criado com sucesso!"
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
 
     def edit
+      @template = ::Template.find(params[:id])
     end
 
     def update
-      flash[:notice] = "Template atualizado (placeholder)"
-      redirect_to admin_templates_path
+      @template = ::Template.find(params[:id])
+      if @template.update(template_params)
+        redirect_to admin_templates_path, notice: "Template atualizado!"
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
 
     def destroy
-      flash[:alert] = "Template deletado (placeholder)"
-      redirect_to admin_templates_path
+      @template = ::Template.find(params[:id])
+      @template.destroy
+      redirect_to admin_templates_path, notice: "Template removido."
+    end
+
+    private
+
+    def template_params
+      params.require(:template).permit(:nome, :descricao)
     end
   end
 end
--- app/controllers/admin/turmas_controller.rb
+++ app/controllers/admin/turmas_controller.rb
@@ -1,30 +1,49 @@
 module Admin
   class TurmasController < ApplicationController
     def index
+      @turmas = ::Turma.all
     end
 
     def show
+      @turma = ::Turma.find(params[:id])
     end
 
     def new
+      @turma = ::Turma.new
     end
 
     def create
-      flash[:notice] = "Turma criada (placeholder)"
-      redirect_to admin_turmas_path
+      @turma = ::Turma.new(turma_params)
+      if @turma.save
+        redirect_to admin_turmas_path, notice: "Turma criada com sucesso!"
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
 
     def edit
+      @turma = ::Turma.find(params[:id])
     end
 
     def update
-      flash[:notice] = "Turma atualizada (placeholder)"
-      redirect_to admin_turmas_path
+      @turma = ::Turma.find(params[:id])
+      if @turma.update(turma_params)
+        redirect_to admin_turmas_path, notice: "Turma atualizada!"
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
 
     def destroy
-      flash[:alert] = "Turma deletada (placeholder)"
-      redirect_to admin_turmas_path
+      @turma = ::Turma.find(params[:id])
+      @turma.destroy
+      redirect_to admin_turmas_path, notice: "Turma removida."
+    end
+
+    private
+
+    def turma_params
+      params.require(:turma).permit(:codigo, :departamento, :semestre)
     end
   end
 end
